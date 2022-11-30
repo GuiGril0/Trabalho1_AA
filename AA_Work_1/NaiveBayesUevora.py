70f52144a0c07ceabb4feb0888cb4dd0650c7a85
guigrilo <bacanogui@gmail.com>
2022-11-30 12:10:45 +0000

    Finally Done!

--- AA_Work_1/NaiveBayesUevora.py
+++ AA_Work_1/NaiveBayesUevora.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 class NaiveBayesUevora:
     alpha = float
-    # construtor
-    def __int__(self,alpha = 0.0 ):
+
+    #construtor
+
+    def __init__(self,alpha = 0.0 ):
         self.alpha = alpha
         self.colunas = list
         self.numPropriedades = {}      #por coluna
@@ -55,12 +57,22 @@ class NaiveBayesUevora:
                     self.probaXc[atributo][valor][valorY] = (nOcorr + self.alpha)/ \
                         (nOcorrY + (self.alpha * self.numPropriedades[atributo]))
 
+    def addPropriety(self,atributo, propriedade):
+        self.probaXc[atributo][propriedade] = {}
+        for valorY in np.unique(self.yTreino):
+            self.probaXc[atributo][propriedade][valorY] = 0
+            nOcorrY = sum(self.yTreino == valorY)
+            nOcorrX_Y = 0
+            self.probaXc[atributo][propriedade][valorY] = (nOcorrX_Y + self.alpha) / \
+                                                        (nOcorrY + (self.alpha * self.numPropriedades[atributo]))
+    """
         # P(b)
         for atributo in self.colunas:
             nOcorrX = self.xTreino[atributo].value_counts().to_dict()
             for valor, nOcorr in nOcorrX.items():
                 self.probaX[atributo][valor] = (nOcorr + self.alpha)/ \
-                   (self.treinoSize + self.alpha + self.numPropriedades[atributo])
+                   (self.treinoSize + self.alpha + self.numPropriedades[atributo]) 
+    """
 
     def predict(self, x):
         resultados = []
@@ -70,30 +82,48 @@ class NaiveBayesUevora:
             for valorY in np.unique(self.yTreino):
                 Pa = self.classes[valorY]
                 Pba = 1
-                Pb = 1
                 for atributo, propriedade in zip(self.colunas, teste):
-                    #print(f'{atributo},{propriedade},{valorY}')
+                    if propriedade not in np.unique(self.xTreino[atributo]):
+                        self.addPropriety(atributo, propriedade)
                     Pba *= self.probaXc[atributo][propriedade][valorY]
-                    Pb *= self.probaX[atributo][propriedade]
                 possiveisResultados[valorY] = Pba * Pa  # (Pba*Pa)/Pb
-            #print(possiveisResultados)
             resultado = max(possiveisResultados,key=lambda x: possiveisResultados[x])
             resultados.append(resultado)
+            #resultados.append(possiveisResultados)
        # print(resultados)
         return np.array(resultados)
-    def acuracy_score(self, x, y):
+    def accuracy_score(self, x, y):
         prev = self.predict(x)
         return round(float((sum(prev == y))/ float(len(y))*100),2)
-    #def precision_score(self, x, y):
-
-
+    def precision_score(self, x, y):
+        resultados =[]
+        prev = self.predict(x)
+        vp = 0
+        fp = 0
+        for valorY in np.unique(self.yTreino):
+            for vy , vyp in zip(y, prev):
+               # print(f'{vy},{vyp},{valorY}')
+                if vy == vyp and vyp == valorY:
+                    vp +=1
+                elif vy != valorY and valorY == vyp:
+                    fp +=1
+            if vp == 0 or fp == 0:
+                resultados.append(0)
+            else:
+                resultados.append(vp / (vp + fp))
+            print(resultados)
+        return round(float(sum(resultados)/len(resultados)*100),2)
 
 
 nb = NaiveBayesUevora()
-file= pd.read_csv("Dados/breast-cancer-test2.csv")
+file = pd.read_csv("Dados/breast-cancer-train.csv")
 x = file.drop([file.columns[-1]], axis= 1)
 y = file[file.columns[-1]]
-nb.__int__()
+nb.__init__()
 nb.fit(x,y)
-#print(nb.precision_score(x, y))
-print(nb.acuracy_score(x,y))
+file = pd.read_csv("Dados/breast-cancer-test.csv")
+x1 = file.drop([file.columns[-1]], axis= 1)
+y2 = file[file.columns[-1]]
+print(f'Predição: {nb.predict(x1)}')
+print(f'Precisão: {nb.precision_score(x1, y2)}')
+print(f'Exatidão: {nb.accuracy_score(x1,y2)}')
--- AA_Work_1/main.py
+++ AA_Work_1/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-#import NaiveBayesUevora
+import NaiveBayesUevora
 
 from os import path
 
@@ -39,21 +39,50 @@ def main():
 
     X, y = process_data(data, column)
 
-    nb = NaiveBayesUevora()
+    nb = NaiveBayesUevora.NaiveBayesUevora()
     nb.__init__(alpha)
     nb.fit(X, y)
 
-    print(data.rows)
-
     while True:
-        test = input("Introduza o ficheiro de teste: ")
-        if path.isfile(test):
+        choice = input("Deseja utilizar terminal ou ficheiro? ")
+        if choice.lower() in ["terminal", "t", "ficheiro", "ficheiros", "f"]:
             break
-        print("O input introduzido não corresponde a um ficheiro existente!")
+        print("Introduza uma resposta válida!")
 
-    d = get_content(test)
+    if choice.lower() in ["ficheiro", "ficheiros", "f"]:
+        while True:
+            test = input("Introduza o ficheiro de teste: ")
+            if path.isfile(test):
+                break
+            print("O input introduzido não corresponde a um ficheiro existente!")
+        d = get_content(test)
+    else:
+        d = []
+        while True:
+            print("Introduza as queries no terminal. Quando desejar terminar introduza 'exit' ou 'sair'.")
+            aux = []
+            for i in X:
+                test = input(i + ": ")
+                if test.lower() in ["exit", "sair"]:
+                    break
+                aux.append(test)
+            if test.lower() in ["exit", "sair"]:
+                break
+            d.append(aux)
+
+            i = 0;
+            while i < len(data.loc[:, column]):
+                line = X.iloc[i]
+                if not all(item in line for item in d):
+                    nb.addFit(d)
+                ++i
+
+
+    print(d)
 
     dX, dy = process_data(d, column)
+
+    compare_datas(data, d)
 
     nb.predict(dX)
 
@@ -65,120 +94,14 @@ def check_column(data, column):
     return column in data.columns
 
 def process_data(data, c):
-    print(data)
-    X = data.drop([data.columns[c]], axis=1)
-    y = data[data.columns[c]]
+    #print(data)
+    #X = data.drop([data.columns[c]], axis=1)
+    X = data.drop(columns=[c], axis=1)
+    y = data.loc[:, c]
     return X, y
 
 def compare_datas(data, d):
-    test = []
-    training = []
-
-
-
-
-class NaiveBayesUevora:
-    alpha = 0.0
-
-    #construtor
-    def __init__(self, alpha = 0.0):
-        self.alpha = alpha
-        self.propriedades = list
-        self.propNumVar = {}
-        self.numOcorrencias = {}
-        self.clasProb= {}
-        self.PropriedadesProb = {}
-
-        self.x_treino = np.array
-        self.y_treino = np.array
-        self.treinoSize = int
-        self.numPropriedades = int
-
-        """
-        função para gerar um classificador
-        a partir de um conjunto de treino
-        """
-    def fit(self, x, y):
-        self.propriedades = list(x.columns)
-        self.x_treino = x
-        self.y_treino = y
-        self.treinoSize = x.shape[0] #numero de linhas
-        self.numPropriedades = x.shape[1]   #numero de colunas
-        #print(self.propriedades)
-        for prop in self.propriedades:
-            self.numOcorrencias[prop] = {}
-            self.PropriedadesProb[prop] = {}
-            self.propNumVar[prop] = len(np.unique(self.x_treino[prop]))
-            #print(f'{prop}--{len(np.unique(self.x_treino[prop]))}')
-
-            for var  in np.unique(self.x_treino[prop]):
-                self.PropriedadesProb[prop].update({var: 0})
-
-                for var_final in np.unique(self.y_treino):
-                    self.numOcorrencias[prop].update({var+"_"+var_final: 0})
-                    self.clasProb.update({var_final: 0})
-
-        for var_final in np.unique(self.y_treino):
-            #print(var_final)
-            count = sum(self.y_treino == var_final)
-            self.clasProb[var_final] = (count + self.alpha)/(self.treinoSize + (self.alpha * len(np.unique(self.y_treino))  ) )
-            #print((count + self.alpha)/(self.treinoSize + (self.alpha * len(np.unique(self.y_treino)))))
-        for prop in self.propriedades:
-
-            for var_final in np.unique(self.y_treino):
-                count_total = sum(self.y_treino == var_final)
-                prop_proba = self.x_treino[prop][self.y_treino[self.y_treino == var_final].index.values.tolist()].value_counts().to_dict()
-                for prop_val, count in prop_proba.items():
-                    self.numOcorrencias[prop][prop_val+"_"+var_final] = (count +self.alpha) /(count_total + (self.alpha * self.propNumVar[prop]))
-
-        for prop in self.propriedades:
-            prop_vals = self.x_treino[prop].value_counts().to_dict()
-
-            for prop_val, count in prop_vals.items():
-                self.PropriedadesProb[prop][prop_val] = (count +self.alpha)/ (self.treinoSize + (self.alpha + self.propNumVar[prop] ))
-    """
-    função para previsões em função
-    de um conjunto de dados de teste,
-    com base no classificador obtido
-    na função fit(x, y)
-    """
-    def predict(self, X):
-        results = []
-        #X = np.array(X)
-        print(type(X))
-        for query in X:
-            print(query)
-            probs_outcome = {}
-            for prop_vals in np.unique(self.y_treino):
-                prior = self.clasProb[prop_vals]
-                probabilidade = 1
-                evidencia = 1
-
-                for prop, prop_val in zip(self.propriedades, query):
-                    probabilidade *= self.numOcorrencias[prop][prop_val+"_"+prop_vals]
-                    evidencia *= self.PropriedadesProb[prop][prop_val]
-
-                posterior = (probabilidade * prior) / evidencia
-
-                probs_outcome[prop_vals] = posterior
-
-            result = max(probs_outcome, key=lambda x: probs_outcome[x])
-            results.append(result)
-        return np.array(results)
-    """
-    função que calcula a exatidão
-    de um classificador dado um
-    conjunto de teste. Retorna um
-    valor de tipo float
-    """
-    #def accuracy_score(x, y):
-
-    """
-    função que serve para calcular a
-    precisão de um classificador dado
-    um conjunto de teste. Retorna um
-    valor de tipo float
-    """
-    #def precision_score(x, y):
+     for i in range(data):
+         print(i)
 
 main()
